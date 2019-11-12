3e2870b3354a143c28a398f193689b70d2d6fd4e
Naruth Kongurai <naruth.kon@truedigital.com>
2019-11-12 18:27:50 +0700

    Added logic for displaying selected item

--- src/components/compare/CompareDetails.jsx
+++ src/components/compare/CompareDetails.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 
 import "./Compare.scss";
 
+import MovieDetails from "../details/MovieDetails";
+
 const CompareDetails = props => {
   const { leftSelection, rightSelection, options } = props;
+
+  const mapSelectionToDetails = selectedId => {
+    options.forEach(item => {
+      if (item.id === selectedId) {
+        return <MovieDetails details={item} />
+      }
+    });
+  }
 
   return (
     <div className="Compare__wrapper">
       <div className="flex-container">
         <div className="flex-item">
-          <p>{leftSelection}</p>
-          {/* <MovieDetail details={leftSelection} */}
+          {mapSelectionToDetails(leftSelection)}
         </div>
         <div className="flex-item">
-          <p>{rightSelection}</p>
-          {/* <MovieDetail details={rightSelection} */}
+          {mapSelectionToDetails(rightSelection)}
         </div>
       </div>
     </div>
